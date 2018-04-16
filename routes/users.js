0ed509bc96322bbf19b390f5b98f5325d020cdfc
Adrian <adriantoddross@gmail.com>
2018-04-16 17:00:23 -0400

    create local POST endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,33 @@
 'use strict';
 
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
 const User = require('../models/user');
 
+router.post('/users/signup/local', (req, res, next) => {
+
+  let { firstname, username, password } = req.body;
+
+  return User.hashPassword(password)
+  .then(digest => {
+    return User.create({
+      local: {
+        firstname,
+        username,
+        password: digest
+      }
+    });
+  })
+  .then(result => {
+    return res.status(201).location(`/users/${result.id}`).json(result);
+  })
+  .catch(err => {
+    if (err.code === 11000) {
+      err = new Error('The username already exists');
+      err.status = 400;
+    }
+    next(err);
+  })
+});
+
+module.exports = router;
