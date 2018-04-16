48909eb2a973d159e3d83e4f94b723240b1984b9
Adrian <adriantoddross@gmail.com>
2018-04-16 15:45:51 -0400

    add schema serialization and user methods for bcrypt

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ import { Collection } from 'mongoose';
 'use strict';
 
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
   { local: {
@@ -21,3 +22,24 @@ const userSchema = new mongoose.Schema(
     }
   }
 );
+
+userSchema.set('toObject', {
+  transform: function (doc, ret) {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+    delete ret.password;
+  }
+});
+
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compare(password, this.local.password);
+};
+
+userSchema.statics.hashPassword = function (password) {
+  return bcrypt.hash(password, 10);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
