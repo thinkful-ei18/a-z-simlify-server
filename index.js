4ed7f370cd9f7178b5c2c7dc22d5a116ca0c8000
Adrian <adriantoddross@gmail.com>
2018-04-17 10:35:39 -0400

    add auth prefix for /login path

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get('/', function (req, res) {
 });
 
 app.use('', usersRouter);
-app.use('', authRouter);
+app.use('auth', authRouter);
 
 passport.use(localStrategy);
 passport.use(jwtStrategy);
