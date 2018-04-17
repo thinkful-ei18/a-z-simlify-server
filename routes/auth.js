ca939ca599a5924e8cad6ba980cbc8424227851a
Adrian <adriantoddross@gmail.com>
2018-04-17 00:16:22 -0400

    return authTokens upon login

--- routes/auth.js
+++ routes/auth.js
@@ -3,13 +3,22 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const {JWT_SECRET, JWT_EXPIRY } = require('../config');
+const jwt = require('jsonwebtoken');
 
 const localAuth = passport.authenticate('local', {session: false, failWithError: true});
+const {JWT_SECRET, JWT_EXPIRY } = require('../config');
+
+function createAuthToken(user) {
+  return jwt.sign({ user }, JWT_SECRET, {
+    subject: user.local.username,
+    expiresIn: JWT_EXPIRY
+  });
+}
 
 router.post('/login', localAuth, function (req, res) {
-  console.info(req.user.username, 'successfully logged in.');
-  return res.json(req.user);
+  console.info(req.user.local.username, 'successfully logged in.');
+  const authToken = createAuthToken(req.user);
+  return res.json({ authToken });
 });
 
 module.exports = router;
