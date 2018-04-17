8649574ef99e7d5ae50f526976cdadd947d9eceb
Adrian <adriantoddross@gmail.com>
2018-04-17 01:20:43 -0400

    require min chars for username; validate min & max chars for password

--- routes/users.js
+++ routes/users.js
@@ -38,6 +38,35 @@ router.post('/signup/local', (req, res, next) => {
     return next(err);
   }
 
+  const sizedFields = {
+    username: { min: 1 },
+    password: { min: 8, max: 72 }
+  };
+
+  const tooSmallField = Object.keys(sizedFields).find(
+    field => 'min' in sizedFields[field] &&
+      req.body[field].trim().length < sizedFields[field].min
+  );
+  if (tooSmallField) {
+    const min = sizedFields[tooSmallField].min;
+    const err = new Error(`Field: '${tooSmallField}' must be at least ${min} characters long`);
+    err.status = 422;
+    return next(err);
+  }
+
+  const tooLargeField = Object.keys(sizedFields).find(
+    field => 'max' in sizedFields[field] &&
+      req.body[field].trim().length > sizedFields[field].max
+  );
+
+  if (tooLargeField) {
+    const max = sizedFields[tooLargeField].max;
+    const err = new Error(`Field: '${tooSmallField}' must be at most ${max} characters long`);
+    err.status = 422;
+    return next(err);
+  }
+
+  /* END OF INPUT VALIDATION */
 
   let { firstname, username, password } = req.body;
 
