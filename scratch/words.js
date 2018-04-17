9657ce5be1dbbc2b72776923d00cfded2806901e
Adrian <adriantoddross@gmail.com>
2018-04-17 14:42:09 -0400

    create 10 question/answer pairs

---
+++ scratch/words.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const words = [
+  {question: 'Sul Sul', answer: 'Hello'},
+  {question: 'Dag Dag', answer: 'Goodbye'},
+  {question: 'Hooba Noobie', answer: 'What\'s up'},
+  {question: 'Litzergam', answer: 'Thank you'},
+  {question: 'Geelfrob', answer: 'See you soon'},
+  {question: 'Whippna Choba Dog', answer: 'This is cool'},
+  {question: 'Boobasnot', answer: 'I don\'t like you'},
+  {question: 'Cuh Teekaloo', answer: 'How\'s it going'},
+  {question: 'Renato', answer: 'Go away'},
+  {question: 'Jowlenin', answer: 'Interesting'},
+  {question: 'Kooj', answer: 'Sweet'},
+];
