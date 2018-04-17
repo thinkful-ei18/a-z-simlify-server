075e8ba99544a45884956a89af560025f3584d26
adriantoddross <adriantoddross@gmail.com>
2018-04-16 22:26:53 -0400

    add indentation

--- routes/users.js
+++ routes/users.js
@@ -9,25 +9,25 @@ router.post('/signup/local', (req, res, next) => {
   let { firstname, username, password } = req.body;
 
   return User.hashPassword(password)
-  .then(digest => {
-    const newUser = {
-      firstname: firstname,
-      username: username,
-      password: digest,
-    }
-    return User.create({local: newUser})
-  })
-  .then(result => {
-    console.log(result);
-    return res.status(201).location(`/users/${result.id}`).json(result);
-  })
-  .catch(err => {
-    if (err.code === 11000) {
-      err = new Error('The username already exists');
-      err.status = 400;
-    }
-    next(err);
-  });
+    .then(digest => {
+      const newUser = {
+        firstname: firstname,
+        username: username,
+        password: digest,
+      };
+      return User.create({local: newUser});
+    })
+    .then(result => {
+      console.log(result);
+      return res.status(201).location(`/users/${result.id}`).json(result);
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('The username already exists');
+        err.status = 400;
+      }
+      next(err);
+    });
 });
 
 module.exports = router;
