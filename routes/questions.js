76ea7d7ccfff954cc9e6ccae910dd6128ecefe31
Adrian <adriantoddross@gmail.com>
2018-04-17 12:06:31 -0400

    add POST endpoint

--- routes/questions.js
+++ routes/questions.js
@@ -7,3 +7,14 @@ router.get('questions', (req, res, next) => {
   return res.json('♪ Sul Sul')
     .catch(err => next(err));
 });
+
+router.post('questions', (req, res, next) => {
+
+  let { answer } = req.body;
+
+  if (answer === 'Hello') {
+    return res.json('Correct! ♪ Sul Sul means "Hello!" in Simlish.');
+  } else {
+    return res.json('Sorry, try again!');
+  }
+});
