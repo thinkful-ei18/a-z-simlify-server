fb970d14b30b325e27cd070f3691e78ae9ca9705
Adrian <adriantoddross@gmail.com>
2018-04-16 23:45:15 -0400

    remove options variable

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-const options = {session: false, failWithError: true};
-const localAuth = passport.authenticate('local', options);
+const localAuth = passport.authenticate('local', {session: false, failWithError: true});
 
 router.post('/login', localAuth, function (req, res) {
   console.info(req.user.username, 'successfully logged in.');
