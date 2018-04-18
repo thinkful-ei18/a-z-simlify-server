3271f1f43b2644ff9290b8feea0bd1132a42f672
Adrian <adriantoddross@gmail.com>
2018-04-18 12:34:56 -0400

    test all singleLinkedList methods

--- ds/SLL.js
+++ ds/SLL.js
@@ -17,6 +17,19 @@ class SingleLinkedList {
     this.head = new Word (word, this.head);
   }
 
+  insertAt(word, M) {
+    let temp = this.head;
+    let counter = 0;
+
+    while (counter < M) {
+      temp = temp.next;
+      counter++;
+    }
+
+    this.head = this.head.next;
+    temp.next = new Word(word, temp.next);
+  }
+
   get() {
     return this.head; // return first question/answer pair
   }
@@ -25,8 +38,10 @@ class SingleLinkedList {
     if (M <= 0) {
       return null;
     }
-
+    console.log('M:', word.M);
     const foundWord = this.find(M - 1);
+    console.log('foundword:',foundWord);
+
     const newWord = new Word(word, null);
     newWord.next = foundWord.next;
     foundWord.next = newWord;
@@ -55,7 +70,7 @@ class SingleLinkedList {
     let current = this.head;
     let pretty = '';
     while (current) {
-      pretty += `=> Question: ${current.question}, Answer:${current.answer}, M:${current.M} `;
+      pretty += `=> Question: ${current.question}, Answer:${current.answer}, M:${current.M}, next: ${current.next} `;
       current = current.next;
     }
     console.log(pretty);
@@ -66,8 +81,8 @@ class SingleLinkedList {
 
 const words = [
   {question: 'Sul Sul', answer: 'Hello', M:1},
-  {question: 'Dag Dag', answer: 'Goodbye', M:1},
   {question: 'Hooba Noobie', answer: 'What\'s up', M:1},
+  {question: 'Dag Dag', answer: 'Goodbye', M:1},
   {question: 'Litzergam', answer: 'Thank you', M:1},
   {question: 'Geelfrob', answer: 'See you soon', M:1},
   {question: 'Whippna Choba Dog', answer: 'This is cool', M:1},
@@ -75,12 +90,20 @@ const words = [
   {question: 'Cuh Teekaloo', answer: 'How\'s it going', M:1},
   {question: 'Renato', answer: 'Go away', M:1},
   {question: 'Jowlenin', answer: 'Interesting', M:1},
-  {question: 'Kooj', answer: 'Sweet', M:1},
+  {question: 'Kooj', answer: 'Sweet', M:3},
 ];
 
 function main() {
   let sll = new SingleLinkedList();
   words.map(word => sll.insertFirst(word));
+  // console.log(sll);
+  sll.print();
+  console.log('*********************************************');
+  // console.log(sll.get());
+  // console.log(sll.find(words[7]));
+  // sll.push(sll.get(), sll.get().M);
+  sll.insertAt(sll.get(), sll.get().M);
+  // console.log(sll);
   sll.print();
 }
 main();
