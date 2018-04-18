9306d021b944af2f2895b3bc4adfb4ddbf41b512
Zhou <540376482yzb@gmail.com>
2018-04-18 12:52:20 -0700

    add random selection when generate

--- ds/SLL.js
+++ ds/SLL.js
@@ -1,108 +1,106 @@
-'use strict';
+'use strict'
 class Word {
-  constructor(word, next=null) {
-    this.question = word.question;
-    this.answer = word.answer;
-    this.M = word.M;
-    this.next = next;
+  constructor(word, next = null) {
+    this.question = word.question
+    this.answer = word.answer
+    this.M = word.M
+    this.next = next
   }
 }
 
 class SingleLinkedList {
   constructor() {
-    this.head = null;
+    this.head = null
   }
 
   insertFirst(word) {
-    this.head = new Word (word, this.head);
+    this.head = new Word(word, this.head)
   }
 
   print() {
-    let current = this.head;
-    let pretty = '';
+    let current = this.head
+    let pretty = ''
     while (current) {
-      pretty += `=> Question: ${current.question}, Answer:${current.answer}, M:${current.M}, next: ${current.next} `;
-      current = current.next;
+      pretty += `=> Question: ${current.question}, Answer:${current.answer}, M:${current.M}, next: ${current.next} `
+      current = current.next
     }
-    console.log(pretty);
+    console.log(pretty)
   }
-
 }
 
-function createWord(word, next=null) {
-
+function createWord(word, next = null) {
   return {
     question: word.question,
     answer: word.answer,
     M: word.M,
-    next: next};
+    next: next
+  }
 }
 
 function insertAt(words, word, M) {
-
-  let temp = words.head;
-  let counter = 0;
+  let temp = words.head
+  let counter = 0
 
   if (M > 9) {
-    insertLast(words, word);
+    insertLast(words, word)
   }
 
   while (counter < M) {
     if (temp.next === null) {
-      return null;
+      return null
     }
 
-    temp = temp.next;
-    counter++;
+    temp = temp.next
+    counter++
   }
 
-  words.head = words.head.next;
-  temp.next = createWord(word, temp.next);
+  words.head = words.head.next
+  temp.next = createWord(word, temp.next)
 
-  counter = 0;
-  temp = words.head;
+  counter = 0
+  temp = words.head
 
-  while(counter < 9) {
-    temp = temp.next;
-    counter++;
+  while (counter < 9) {
+    temp = temp.next
+    counter++
   }
-  temp.next = null;
+  temp.next = null
 }
 
 function insertLast(words, word) {
-  let node = words.head;
+  let node = words.head
 
   while (node.next !== null) {
-    node = node.next;
+    node = node.next
   }
 
-  node.next = createWord(word);
+  node.next = createWord(word)
 }
 
-module.exports = {SingleLinkedList, insertAt, createWord, insertLast};
+module.exports = { SingleLinkedList, insertAt, createWord, insertLast }
 
 const words = [
-  {question: 'Sul Sul', answer: 'Hello', M:1},
-  {question: 'Hooba Noobie', answer: 'What\'s up', M:1},
-  {question: 'Dag Dag', answer: 'Goodbye', M:1},
-  {question: 'Litzergam', answer: 'Thank you', M:1},
-  {question: 'Geelfrob', answer: 'See you soon', M:1},
-  {question: 'Whippna Choba Dog', answer: 'This is cool', M:1},
-  {question: 'Boobasnot', answer: 'I don\'t like you', M:1},
-  {question: 'Cuh Teekaloo', answer: 'How\'s it going', M:1},
-  {question: 'Renato', answer: 'Go away', M:19},
-  {question: 'Jowlenin', answer: 'Interesting', M:1},
-  {question: 'Kooj', answer: 'Sweet', M:1},
-];
+  { question: 'Sul Sul', answer: 'Hello', M: 1 },
+  { question: 'Hooba Noobie', answer: 'What\'s up', M: 1 },
+  { question: 'Dag Dag', answer: 'Goodbye', M: 1 },
+  { question: 'Litzergam', answer: 'Thank you', M: 1 },
+  { question: 'Geelfrob', answer: 'See you soon', M: 1 },
+  { question: 'Whippna Choba Dog', answer: 'This is cool', M: 1 },
+  { question: 'Boobasnot', answer: 'I don\'t like you', M: 1 },
+  { question: 'Cuh Teekaloo', answer: 'How\'s it going', M: 1 },
+  { question: 'Renato', answer: 'Go away', M: 19 },
+  { question: 'Jowlenin', answer: 'Interesting', M: 1 },
+  { question: 'Kooj', answer: 'Sweet', M: 1 }
+]
 
 function main() {
-  let sll = new SingleLinkedList();
-  words.map(word => sll.insertFirst(word));
+  let sll = new SingleLinkedList()
+  words.map(word => sll.insertFirst(word))
 
-  sll.print();
-  console.log('*********************************************');
-  insertLast(sll, words[3]);
-  sll.print();
+  sll.print()
+  console.log('*********************************************')
+  insertLast(sll, words[3])
+  sll.print()
   // insertAt(sll, sll.head, sll.head.M);
   // console.log(sll);
   // sll.print();
@@ -118,4 +116,4 @@ function main() {
 
   // console.log(sll);
 }
-main();
+// main();
--- routes/questions.js
+++ routes/questions.js
@@ -1,69 +1,86 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const router = express.Router();
-const User = require('../models/user');
-const jwtDecode = require('jwt-decode');
-const {SingleLinkedList, insertAt, createWord, insertLast} = require('../ds/SLL');
-const questions = require('../db/seed/wordbank.json');
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+const jwtDecode = require('jwt-decode')
+const { SingleLinkedList, insertAt, createWord, insertLast } = require('../ds/SLL')
+const questions = require('../db/seed/wordbank.json')
 
-function getUsername (request) {
-  const authToken = request.headers.authorization.split(' ')[1];
-  const currentUser = jwtDecode(authToken).user.local.username;
-  return currentUser;
+function getUsername(request) {
+  const authToken = request.headers.authorization.split(' ')[1]
+  const currentUser = jwtDecode(authToken).user.local.username
+  return currentUser
 }
-
+// return random 10 wordpairs
+function getRandomWords(questions) {
+  const max = questions.length
+  const result = []
+  const indexArr = []
+  let index = null
+  let wordPair = null
+  while (result.length < 10) {
+    index = Math.floor(Math.random() * max)
+    wordPair = questions[index]
+    if (!indexArr.includes(index)) {
+      result.push(wordPair)
+    }
+  }
+  return result
+}
 router.get('/generate', (req, res, next) => {
-  const username = getUsername(req);
+  const username = getUsername(req)
 
-  let sll = new SingleLinkedList();
-  User.findOne({'local.username': username})
+  let sll = new SingleLinkedList()
+  User.findOne({ 'local.username': username })
     .then(user => {
-      user.local.words = sll;
-      questions.forEach(word => sll.insertFirst(word));
+      user.local.words = sll
       // add math.random to grab random group of words from the array
-      return User.findOneAndUpdate({'local.username': username}, {'local.words': user.local.words}, {new: true});
+      const questionSet = getRandomWords(questions)
+      questionSet.forEach(word => sll.insertFirst(word))
+      return User.findOneAndUpdate({ 'local.username': username }, { 'local.words': user.local.words }, { new: true })
     })
     .then(result => {
       if (!result) {
-        throw new Error('Could not generate words for user');
+        throw new Error('Could not generate words for user')
       }
-      return res.json(result);
+      return res.json(result)
     })
-    .catch(err => next(err));
-});
+    .catch(err => next(err))
+})
 
 router.get('/question', (req, res, next) => {
-  return res.json('What does ♪ Sul Sul mean?');
-});
+  return res.json('What does ♪ Sul Sul mean?')
+})
 
 router.post('/answer', (req, res, next) => {
-  let { answer } = req.body;
-  const username = getUsername(req);
-  let feedback = null;
+  let { answer } = req.body
+  const username = getUsername(req)
+  let feedback = null
 
-  User.findOne({'local.username': username})
+  User.findOne({ 'local.username': username })
     .then(user => {
-      const words = user.local.words;
-      const currentWord = words.head;
-      let mIndex = words.head.M;
+      const words = user.local.words
+      const currentWord = words.head
+      let mIndex = words.head.M
 
       if (answer === 'Hello') {
-        feedback = 'Correct! ♪ Sul Sul means "Hello!" in Simlish.';
-        words.head.M = 10;
+        feedback = 'Correct! ♪ Sul Sul means "Hello!" in Simlish.'
+        words.head.M = 10
       } else {
-        feedback = 'Try again';
-        words.head.M = 2;
+        feedback = 'Try again'
+        words.head.M = 2
       }
 
-      insertAt(words, currentWord, mIndex);
-      return User.findOneAndUpdate({'local.username': username}, {'local.words': words}, {new: true});
+      insertAt(words, currentWord, mIndex)
+      return User.findOneAndUpdate({ 'local.username': username }, { 'local.words': words }, { new: true })
     })
     .then(() => {
-      return res.json(feedback);
+      return res.json(feedback)
     })
-    .catch(err => {next(err);});
+    .catch(err => {
+      next(err)
+    })
+})
 
-});
-
-module.exports = router;
+module.exports = router
