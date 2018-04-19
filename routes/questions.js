94f61a1919d4af50c976e62e4e16f565e556aa38
Zhou <540376482yzb@gmail.com>
2018-04-19 14:44:11 -0700

    feedback

--- routes/questions.js
+++ routes/questions.js
@@ -110,8 +110,8 @@ router.post('/answer', (req, res, next) => {
       insertAt(words, currentWord, mIndex)
       return User.findOneAndUpdate({ 'local.username': username }, { 'local.words': words }, { new: true })
     })
-    .then(results => {
-      return res.json(results)
+    .then(() => {
+      return res.json(feedback)
     })
     .catch(err => {
       next(err)
