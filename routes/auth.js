e495dfe286a9d136baedd37825a607e329258ab2
Zhou <540376482yzb@gmail.com>
2018-04-19 08:17:11 -0700

    sanitized auth token

--- routes/auth.js
+++ routes/auth.js
@@ -1,30 +1,34 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const router = express.Router();
-const passport = require('passport');
-const jwt = require('jsonwebtoken');
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+const jwt = require('jsonwebtoken')
 
-const localAuth = passport.authenticate('local', {session: false, failWithError: true});
-const jwtAuth = passport.authenticate('jwt', { session: false, failWithError: true });
-const {JWT_SECRET, JWT_EXPIRY } = require('../config');
+const localAuth = passport.authenticate('local', { session: false, failWithError: true })
+const jwtAuth = passport.authenticate('jwt', { session: false, failWithError: true })
+const { JWT_SECRET, JWT_EXPIRY } = require('../config')
 
 function createAuthToken(user) {
   return jwt.sign({ user }, JWT_SECRET, {
-    subject: user.local.username,
+    subject: user.username,
     expiresIn: JWT_EXPIRY
-  });
+  })
 }
 
-router.post('/login', localAuth, function (req, res) {
-  console.info(req.user.local.username, 'successfully logged in.');
-  const authToken = createAuthToken(req.user);
-  return res.json({ authToken });
-});
+router.post('/login', localAuth, function(req, res) {
+  console.info(req.user.local.username, 'successfully logged in.')
+  const { id } = req.user
+  const { firstname, username } = req.user.local
+  const user = { id, firstname, username }
+  console.log(user)
+  const authToken = createAuthToken(user)
+  return res.json({ authToken })
+})
 
 router.post('/refresh', jwtAuth, (req, res) => {
-  const authToken = createAuthToken(req.user);
-  res.json({ authToken });
-});
+  const authToken = createAuthToken(req.user)
+  res.json({ authToken })
+})
 
-module.exports = router;
+module.exports = router
